8335850d996cdc18616cd805eb4b834e4dab6737
Nazmul Hoda <cse.nazmul.bubt@gmail.com>
2023-03-15 21:46:21 +0600

    [Add] Project Structure

---
+++ GitHub-RepositorySearch/GitHub_RepositorySearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_RepositorySearchApp.swift
+//  GitHub-RepositorySearch
+//
+//  Created by Nazmul on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_RepositorySearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
